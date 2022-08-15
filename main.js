306075b9a75ce72a988f72dc1db0122180ccffcc
Básthy Ádám László <basthyadi@gmail.com>
2022-08-15 17:54:38 +0200

    Add Event to send a welcome message to client

--- main.js
+++ main.js
@@ -3,18 +3,25 @@ var repo = require("./PlayerRepository");
 var player = require("./Player");
 var logic = require("./Logic");
 
-var ingameLogic = new logic.Logic();
-
 var players = new repo.PlayerRepository();
-
-players.AddPlayer();
-
+var ingameLogic = new logic.Logic(players);
 
 var server = new connectionHandler.ConnectionHandler(3000, 2);
 
-server.RegisterMiddleWare('ActiveInput', (data) => {
-
-    console.log(data);
-
+server.RegisterMiddleWare("ActiveInput", (data) => {
+  console.log(data);
+  ingameLogic.HandleInput(data);
 });
 
+server.SubscribeOnConnected((socket) => {
+  players.AddPlayer(new player.Player(socket.id));
+  let playerid = players.GetPlayers().length;
+
+  server.SendToOneClient(socket.id, "welcome", { playerId: playerid });
+});
+
+server.SubscribeOnDisconnected((socket) => {
+  players.RemovePlayerById(socket.id);
+});
+
+ingameLogic.StartGame();
