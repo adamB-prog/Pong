a648ba4becf4a0589e6bc191038c8b9a4805fc1f
Básthy Ádám László <basthyadi@gmail.com>
2022-08-15 17:53:53 +0200

    Code rearrangement

--- Player.js
+++ Player.js
@@ -1,47 +1,41 @@
-class Player
-{
-  #socket;
-  #lastInput;
-  #y;
-  #vy;
-  #a;
+class Player {
+  socketid;
+  lastInput;
+  y;
+  vy;
+  a;
   #point;
-  constructor(socket)
-  {
-    this.socket = socket;
-    
-    this.#y = 0.5;
-    this.#vy = 0;
-    this.#a = 0.01;
-    this.#point = 0;
+  constructor(socketid) {
+    this.socketid = socketid;
+
+    this.y = 0.5;
+    this.vy = 0;
+    this.a = 0.01;
+    this.point = 0;
   }
-  Tick()
-  {
-    this.#y = this.Clamp(this.#y + this.#vy, 0, 1);
+  Tick() {
+    this.y = this.Clamp(this.y + this.vy, 0, 1);
   }
-  AddPoint(amount)
-  {
-    this.#point += amount;
+  AddPoint(amount) {
+    this.point += amount;
   }
 
-  MoveDown()
-  {
-    this.#vy += this.#a;
+  MoveDown() {
+    this.vy += this.a;
   }
 
-  MoveUp()
-  {
-    this.#vy -= this.#a;
+  MoveUp() {
+    this.vy -= this.a;
   }
-  StopMovement()
-  {
-    this.#vy = 0;
+  StopMovement() {
+    this.vy = 0;
   }
-  
-  Clamp(num, min, max)
-  {
+  GetSocketId() {
+    return this.socketid;
+  }
+  Clamp(num, min, max) {
     return num <= min ? min : num >= max ? max : num;
   }
 }
 
-module.exports = {Player};
+module.exports = { Player };
