9eb978a40f9d8aa9b32a02741894595d06f92bec
Básthy Ádám László <basthyadi@gmail.com>
2022-08-16 23:05:21 +0200

    Add Start game condition and handle player movement

--- main.js
+++ main.js
@@ -5,12 +5,19 @@ var logic = require("./Logic");
 
 var players = new repo.PlayerRepository();
 var ingameLogic = new logic.Logic(players);
-
 var server = new connectionHandler.ConnectionHandler(3000, 2);
 
 server.RegisterMiddleWare("ActiveInput", (data) => {
   console.log(data);
   ingameLogic.HandleInput(data);
+  let playerID = players
+    .GetPlayers()
+    .findIndex((player) => player.GetSocketId() == data.clientid);
+
+  server.SendToEveryone("move", {
+    clientIndex: playerID + 1,
+    movement: data.data,
+  });
 });
 
 server.SubscribeOnConnected((socket) => {
@@ -20,8 +27,12 @@ server.SubscribeOnConnected((socket) => {
   server.SendToOneClient(socket.id, "welcome", { playerId: playerid });
 });
 
+server.SubscribeOnConnected(() => {
+  if (players.GetPlayers().length == 2) {
+    ingameLogic.StartGame();
+  }
+});
+
 server.SubscribeOnDisconnected((socket) => {
   players.RemovePlayerById(socket.id);
 });
-
-ingameLogic.StartGame();
