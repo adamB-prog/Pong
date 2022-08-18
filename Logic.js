691a8c9055b8f4879ebcf64f11a2fd3201511ffb
Básthy Ádám László <basthyadi@gmail.com>
2022-08-18 12:06:13 +0200

    Add Observer pattern to Logic when movement happening

--- Logic.js
+++ Logic.js
@@ -1,16 +1,24 @@
 class Logic {
+  #onMove;
   constructor(playerRepository) {
     this.playerRepository = playerRepository;
+    this.#onMove = [];
     this.endGame = true;
     this.gameLoop = null;
   }
   StartGame(tps) {
     console.log("Starting the game");
+    //if any gameloop is in progress, then clear it
+    clearInterval(this.gameLoop);
     this.gameLoop = setInterval(() => this.Tick(), 1000 / 60);
   }
   Tick() {
-    this.playerRepository.players.forEach((player) => {
-      player.Tick();
+    this.playerRepository.players.forEach((player, index) => {
+      //if a player moved, then notify the subscribers
+      let move = player.Tick();
+      if (move) {
+        this.#TriggerOnMove(index);
+      }
     });
   }
   HandleInput(data) {
@@ -22,8 +30,14 @@ class Logic {
     } else {
       findPlayer.StopMovement();
     }
-
-    console.log(findPlayer);
+  }
+  SubscribeOnMove(method) {
+    this.#onMove.push(method);
+  }
+  #TriggerOnMove(index) {
+    this.#onMove.forEach((method) => {
+      method(index);
+    });
   }
 }
 
