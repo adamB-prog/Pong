4556c24a55d785722a5046572f25d6814fffb9de
Básthy Ádám László <basthyadi@gmail.com>
2022-08-18 12:10:28 +0200

    Subscribe to OnMove

--- main.js
+++ main.js
@@ -8,31 +8,30 @@ var ingameLogic = new logic.Logic(players);
 var server = new connectionHandler.ConnectionHandler(3000, 2);
 
 server.RegisterMiddleWare("ActiveInput", (data) => {
-  console.log(data);
   ingameLogic.HandleInput(data);
-  let playerID = players
-    .GetPlayers()
-    .findIndex((player) => player.GetSocketId() == data.clientid);
-
-  server.SendToEveryone("move", {
-    clientIndex: playerID + 1,
-    movement: data.data,
-  });
 });
 
+//add player to the array and welcome it
 server.SubscribeOnConnected((socket) => {
   players.AddPlayer(new player.Player(socket.id));
   let playerid = players.GetPlayers().length;
 
   server.SendToOneClient(socket.id, "welcome", { playerId: playerid });
 });
-
+//if there are enough players then start the game
 server.SubscribeOnConnected(() => {
   if (players.GetPlayers().length == 2) {
     ingameLogic.StartGame();
   }
 });
-
+//remove the disconnected player from the array
 server.SubscribeOnDisconnected((socket) => {
   players.RemovePlayerById(socket.id);
+});
+//send to clients that movement happened and the necessery information to it
+ingameLogic.SubscribeOnMove((index) => {
+  server.SendToEveryone("move", {
+    clientindex: index + 1,
+    y: ingameLogic.playerRepository.GetPlayers()[index].y,
+  });
 });
